9db92679ef671eaeedb3be820f1f7d9165bd5820
Robin Tram <robin.tram@gmail.com>
2018-10-20 18:02:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,7 +12,11 @@ class Dog
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE Dog
+      CREATE TABLE dog(
+        id INTEGER PRIMARY KEY,
+          name TEXT,
+          breed TEXT
+      )
     SQL
     
     DB[:conn].execute(sql)
