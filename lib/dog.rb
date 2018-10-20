6d842fb2f255755b72c9ab971c40fe193fbe6703
Robin Tram <robin.tram@gmail.com>
2018-10-20 18:07:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,5 +30,12 @@ class Dog
     DB[:conn].execute(sql)
   end 
   
-  
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs(name, breed)
+      VALUES (?, ?)
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.breed)
+  end 
 end 
