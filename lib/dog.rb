7e533c61bdf85b14f397b821e39686f363152528
Robin Tram <robin.tram@gmail.com>
2018-10-20 18:09:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,6 +38,6 @@ class Dog
     
     DB[:conn].execute(sql, self.name, self.breed)
     
-    id = DB[:conn].execute("SELECT last_insert_row(")
+    id = DB[:conn].execute("SELECT last_insert_row() from dogs")
   end 
 end 
