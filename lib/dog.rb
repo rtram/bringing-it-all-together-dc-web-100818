7d58afc1389140c1a1ff9dcf2e7da8b16677adbe
Robin Tram <robin.tram@gmail.com>
2018-10-20 18:38:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -78,6 +78,12 @@ class Dog
   end 
   
   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs 
+      WHERE name = ?
+    SQL
     
+    DB[:conn].execute(sql, name)
   end 
 end 
